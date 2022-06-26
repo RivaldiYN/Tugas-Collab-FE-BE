26491ec6c45e8d6a0a8b97eafd614c35a92cdef5
Aldi Nainggolan <88421969+RivaldiYN@users.noreply.github.com>
2022-06-26 20:43:58 +0700

    Update index.php

--- tugasmvc/app/views/home/index.php
+++ tugasmvc/app/views/home/index.php
@@ -4,6 +4,13 @@
             <div class="container">
                 <h1 class="display-4">Selamat Datang di Website Saya</h1>
                 <p>Halo, nama saya <?= $data['nama'] ?>, anda akan melihat daftar nama peminjam motor di SewaMotor.id</p>
+                <ul class="list-group">
+                    <li class="list-group-item active" aria-current="true">An active item</li>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                    <li class="list-group-item">A fourth item</li>
+                    <li class="list-group-item">And a fifth one</li>
+                </ul>
             </div>
         </div>
     </div>
