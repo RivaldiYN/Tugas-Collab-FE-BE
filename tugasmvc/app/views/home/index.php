fec45fa578a83c32f8b85a77c4e6f1382960cb89
BilhaqAD07 <bilhaqad90@gmail.com>
2022-06-28 18:10:48 +0700

    edit capt on homepage

--- tugasmvc/app/views/home/index.php
+++ tugasmvc/app/views/home/index.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="jumbotron jumbotron-fluid mt-4">
             <div class="container">
-                <h1 class="display-4">Selamat Datang di Website Saya</h1>
-                <p>Halo, nama saya <?= $data['nama'] ?>, anda akan melihat daftar nama peminjam motor di SewaMotor.id</p>
+                <h1 class="display-4">Selamat Datang di SewaMotor</h1>
+                <p>Halo semuanya, nama saya <?= $data['nama'] ?>. Di website ini Anda akan melihat daftar pelanggan yang melakukan peminjaman pada SewaMotor.id</p>
             </div>
         </div>
     </div>
